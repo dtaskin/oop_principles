d9d1f73ef07c28c7b841cd0c1ad0865ac12f1295
dtaskin <taskindeniz26@gmail.com>
2022-04-18 21:53:07 -0400

    added practice

---
+++ src/inheritance/FoodTest.java
@@ -0,0 +1,73 @@
+package inheritance;
+
+import java.util.ArrayList;
+
+public class FoodTest {
+    public static void main(String[] args) {
+        /*
+        Create a food object and define below info for it
+        taste = delicious
+        name = sushi
+        isSpicy = true
+        isOrganic = true
+
+        First, print the name of the food
+        Then, print if the food is organic
+        Finally, print the object itself
+         */
+
+        Food food1 = new Food();
+        food1.taste = "delicious"; // direct access
+        food1.name = "sushi";
+        food1.isSpicy = true;
+        food1.setOrganic(true); // indirect access
+
+        System.out.println(food1.name);
+        System.out.println(food1.isOrganic());
+        System.out.println(food1);
+
+         /*
+        Create a vegetable1 object and define below info for it
+        taste = good
+        name = parsley
+        isSpicy = false
+        isOrganic = true
+        color = green
+
+        First, print the name of the vegetable and color of the vegetable
+        Then, print if the vegetable is organic
+        Finally, print the object itself
+         */
+
+        Vegetable vegetable1 = new Vegetable();
+        vegetable1.taste = "good";
+        vegetable1.name = "parsley";
+        vegetable1.isSpicy = false;
+        vegetable1.setOrganic(true);
+        vegetable1.color = "green";
+
+        System.out.println(vegetable1.name);
+        System.out.println(vegetable1.isOrganic());
+        System.out.println(vegetable1);
+
+
+        /*
+        Create another vegetable2 object and define only the name,
+        name = broccoli
+
+        print this object
+         */
+
+        Vegetable vegetable2 = new Vegetable();
+        vegetable2.name = "broccoli";
+
+        System.out.println(vegetable2);
+
+        ArrayList<Food> allFoods = new ArrayList<>();
+        allFoods.add(food1);
+        allFoods.add(vegetable1);
+        allFoods.add(vegetable2);
+//        allFoods.add("Strawberry"); // cannot add Stawberry because it not is the food
+
+    }
+}
